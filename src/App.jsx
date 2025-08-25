ac18fa58d4798d8cc0c84989fc2bca3b3c9c0831
Tejas <kambletc@gmail.com>
2025-08-25 19:15:22 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import SignupPage from './components/SignupPage';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
+const firebaseConfig = {
+    apiKey: "AIzaSyDdUK6B_o2amzkuxZpkvIy1rjTLIakEi_o",
+    authDomain: "helperhub-c77cc.firebaseapp.com",
+    projectId: "helperhub-c77cc",
+    storageBucket: "helperhub-c77cc.firebasestorage.app",
+    messagingSenderId: "137448240850",
+    appId: "1:137448240850:web:25bcc86a97ff8da8d09f0b",
+    measurementId: "G-CJTT7HTJ61"
+  };
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication
+const auth = getAuth(app);
+
+// Initialize Realtime Database
+const database = getDatabase(app);
+
+export { app, auth, database };
